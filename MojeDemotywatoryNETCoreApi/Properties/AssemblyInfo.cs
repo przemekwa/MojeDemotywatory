15b023f8b45bf11faa764c01bf0a62ecdc4641b2
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-08-05 08:24:04 +0200

    Dodanie projektu .NETCore dla Demotywatory Api

---
+++ MojeDemotywatoryNETCoreApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MojeDemotywatoryNETCoreApi")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45b4835d-12e4-4d97-901e-0672d1c7f2d1")]
